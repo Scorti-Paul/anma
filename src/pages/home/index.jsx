90d7dd2996e32d49fd5be90d858121e3edee529a
Scorti-Paul <scortip@gmail.com>
2023-08-25 16:01:22 +0000

    fixes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,9 +4,9 @@ import About from "./components/about";
 
 const Home = () => {
   return (
-    <main className="px-6 md:px-16">
+    <main className="px-2 md:px-16">
       <Showcase />
-      <div className="md:px-12 mx-auto w-full">
+      <div className="px-4 md:px-12 mx-auto w-full">
         <About />
       </div>
     </main>
